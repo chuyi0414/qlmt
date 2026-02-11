cd0c15be6246ae1d1bc23e2b34b94612cdc07e34
Chuyi <chuyiyx183@163.com>
2026-02-11 20:51:53 +0800

    添加单位基类和怪物基类以及大恐怖的空类

---
+++ qlmt/Assets/_Game/Scripts/Entity/Unit/UnitBaseEntity.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+/// <summary>
+/// 单位基类
+/// </summary>
+public class UnitBaseEntity : EntityLogic
+{
+    
+}
